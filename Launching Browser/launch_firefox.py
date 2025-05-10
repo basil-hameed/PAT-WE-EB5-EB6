6bb7a46f80c91da42925be5bdf9f8da735b3e0dc
Basil Hameed <150420259+basil-hameed@users.noreply.github.com>
2025-05-10 12:54:19 +0530

    Delete Launching Browser directory

--- Launching Browser/launch_firefox.py
+++
@@ -1,8 +0,0 @@
-from playwright.sync_api import sync_playwright
-
-with sync_playwright() as p:
-    browser = p.firefox.launch(headless=False) # launches nightly version
-    page = browser.new_page()
-    page.goto("https://www.guvi.in")
-    print(page.title())
-    browser.close()
--- Launching Browser/launch_webkit.py
+++
@@ -1,9 +0,0 @@
-from playwright.sync_api import sync_playwright
-
-with sync_playwright() as p:
-    browser = p.webkit.launch(headless=False) # lauching headed mode
-    page = browser.new_page() # create new page
-
-    page.goto("https://www.guvi.in") # launch guvi website
-    print(page.title()) # get the page title
-    browser.close() # closing browser
